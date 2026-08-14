208508b7878c1c6b7cc5b22e79cfd795c81d4470
Cocode96 <yangcoco96a@naver.com>
2026-08-14 09:27:09 +0900

    feat : Python 과제 종료 명령 처리 추가

--- python_task/python_final_project_양석호/main.py
+++ python_task/python_final_project_양석호/main.py
@@ -1,10 +1,20 @@
-def main():
+def main()->None:
+    while True:
+        command = input("종료하려면 exit 입력: ")
 
-    print("1. 도서 등록")
-    print("2. 전체 도서 조회")
-    print("3. 도서 검색")
-    print("4. 대여/반납 처리")
-    print("5. 종료")
+        if command == "exit":
+            print("종료")
+            break
 
-    return
+
+class Renderer:
+    tmp = ""
+
+
+class delegate:
+    tmp = ""
+
+
+if __name__ == "__main__":
+    main()
 
